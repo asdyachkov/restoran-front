176166cf6a56d28ab65a015d7428f8e1d413e234
godofprog <stepanderdichok@gmail.com>
2023-01-10 03:26:06 +0300

    added rent page

--- src/components/RentATable.vue
+++ src/components/RentATable.vue
@@ -1,8 +1,43 @@
 <template>
   <my-default>
-    <div>
+    <template v-slot:content>
+      <div class="container">
+        <div class="row row-cols-2" style="margin-top: 15%; margin-left: 7%;">
+          <div class="col text-col">
+            <h1>Забронируйте место в зале прямо сейчас!</h1>
+            <p>Уютные зоны с игровой приставкой PS5, самые рейтинговые игры, множество разнообразных настольных игр,
+              в том числе и нарды, для весёлого времяпрепровождения вместе с друзьями, а так же трансляции самых
+              топовых спортивных событий в мире.</p>
+            <a href="/menu">
+              <div class="btn rent-button">
+                <span>Просмотреть наше меню</span>
+              </div>
+            </a>
+          </div>
+        <div class="row">
+          <form method="post" class="form-style">
+            <h5>Введите Ваше имя*</h5>
+            <input type="text" class="text-input" placeholder="Например: Иван/Ольга">
+            <h5>Выберите количество гостей*</h5>
 
-    </div>
+            <input type="range" id="rangevalue" v-model="value" min="1" max="16" step="1">
+            <output id="rangevalue">{{this.value}}</output>
+
+            <h5>Выберите дату посещения*</h5>
+            <input type="date" class="text-input" placeholder="Например: 25.12.2022">
+            <h5>Напишите примерное время посещения*</h5>
+            <input type="time" class="text-input" placeholder="Например: 16:00">
+            <h5>Введите Ваш номер телефона*</h5>
+            <input type="text" class="text-input phone" placeholder="+7(999)999-99-99">
+            <button class="btn rent-button" type="submit">
+              <span>Забронировать</span>
+            </button>
+            <span></span>
+          </form>
+        </div>
+      </div>
+      </div>
+    </template>
   </my-default>
 </template>
 
@@ -14,7 +49,7 @@ export default {
 
   data() {
     return {
-
+      value: 1,
     }
   },
   methods: {
--- src/components/layouts/MyDefault.vue
+++ src/components/layouts/MyDefault.vue
@@ -4,12 +4,23 @@
     <div class="no_footer">
       <div class="first-screen">
         <header class="container">
-          <div class="d-flex justify-content-center py-3">
-            <ul class="nav nav-pills">
+          <div class="d-flex flex-wrap align-items-center py-3 mb-4 border-bottom">
+            <ul class="nav">
               <li class="nav-item"><router-link to="/" exact class="nav-link">Home</router-link></li>
               <li class="nav-item"><router-link to="/menu" class="nav-link">Menu</router-link></li>
               <li class="nav-item"><router-link to="/rent_a_table" class="nav-link">Rent a table</router-link></li>
             </ul>
+            <div style="margin-left: auto; display: flex; align-items: center; justify-content: center">
+
+              <form role="search">
+                <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+              </form>
+              <button type="button" class="btn btn-warning m-2" v-if="!logged_in">Login</button>
+              <button type="button" class="btn btn-warning m-2" v-if="!logged_in">Sign-up</button>
+              <button type="button" class="btn m-2" v-if="logged_in"><router-link to="#"><img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"></router-link></button>
+              <button type="button" class="btn btn-warning m-2" v-if="logged_in">Logout</button>
+
+            </div>
           </div>
         </header>
 
@@ -48,7 +59,7 @@ export default {
   name: "MyDefault",
   data() {
     return {
-
+      logged_in: false
     }
   },
   methods: {
